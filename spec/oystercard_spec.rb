8b2740bc0a8af4c2bf522af96ea92d6947deed78
omajul <omajul2@hotmail.com>
2016-04-18 19:28:35 +0000

    Steps 4 and 5 - Adding balance and top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard){ described_class.new }
+  
   it "has a balance of 0 when initialized" do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
+  
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
+  
 end
