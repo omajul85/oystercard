fbcbc73c9987aad3772bf6a5a7086e120769bf17
Omar Alvarez <omajul2@hotmail.com>
2016-04-21 11:45:30 +0000

    (WIP) Step 15 - Add some functionalities to JourneyLog

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,5 +1,5 @@
 class JourneyLog
-  attr_reader :journeys
+  # attr_reader :journeys
   
   def initialize(journey_class)
     @journey_class = journey_class
@@ -7,6 +7,20 @@ class JourneyLog
   end
   
   def start(station)
-    @journeys << @journey_class.new(station)
+    @journeys << @journey_class.new
   end
+  
+  def finish(station)
+    puts "Finish the journey"
+    current_journey.finish(station)
+  end
+  
+  def journeys
+    p @journeys.dup
+  end
+  
+  private 
+    def current_journey
+      @journey_class || @journey_class.new
+    end
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,20 +2,40 @@ require 'journey_log'
 
 describe JourneyLog do
   let(:journey) { double(:journey, entry_station: entry_station, completed?: false) }
-  let(:journey_class) { double(:journey_class, new: journey, start: journey)}
+  let(:journey_class) { double(:journey_class, new: journey, start: journey, finish: self) }
   let(:entry_station) { double(:station, name: "A", zone: 1) }
   let(:exit_station) { double(:station, name: "B", zone: 1) }
   subject(:journey_log) { described_class.new(journey_class) }
   
   describe "#start" do
-    it "starts a new journey" do
-      expect(journey_class).to receive(:new).with(entry_station)
-      journey_log.start entry_station
-    end
-    
-    it 'records a journey' do
+    it 'starts and records a journey' do
       journey_log.start(entry_station)
       expect(journey_log.journeys).to include(journey)
+    end
+  end
+  
+  describe "#finish" do
+    before do 
+      journey_log.start(entry_station)
+    end
+    
+    it 'finish a journey' do
+      journey_log.finish(exit_station)
+      expect(journey_log.journeys).to include(journey)
+    end
+  end
+  
+  describe "#journeys" do 
+    before do 
+      5.times {
+        journey_log.start(entry_station)
+        journey_log.finish(exit_station)
+      }
+    end
+    
+    it "prints the list of journeys" do 
+      journey_log.journeys
+      # expect(journey_log.journeys).to be_a(Array)
     end
   end
 end
