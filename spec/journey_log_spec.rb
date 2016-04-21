275dca63d05d923cc41bc4b9f6f36409f67ff526
Omar Alvarez <omajul2@hotmail.com>
2016-04-21 14:05:24 +0000

    (WIP) Step 15 - More refactoring on JourneyLog

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,25 +1,25 @@
 require 'journey_log'
 
 describe JourneyLog do
-  let(:journey) { double(:journey, entry_station: entry_station, completed?: false) }
-  let(:journey_class) { double(:journey_class, new: journey, start: journey, finish: self) }
+  let(:journey) { double(:journey, entry_station: entry_station, completed?: false, start: entry_station, finish: nil) }
+  let(:journey_class) { double(:journey_class, new: journey, start_journey: entry_station, finish_journey: self) }
   let(:entry_station) { double(:station, name: "A", zone: 1) }
   let(:exit_station) { double(:station, name: "B", zone: 1) }
   subject(:journey_log) { described_class.new(journey_class) }
   
   describe "#start" do
-    it 'starts and records a journey' do
-      journey_log.start_journey(entry_station)
-      expect(journey_log.journeys).to include(journey)
+    it 'starts a journey' do
+      expect(journey_log.start_journey(entry_station)).to eq entry_station
     end
   end
   
-  describe "#finish" do
+  describe "#finish and records a journey" do
     before do 
       journey_log.start_journey(entry_station)
     end
     
     it 'finish a journey' do
+      allow(journey).to receive(:finish).with(exit_station).and_return(journey)
       journey_log.finish_journey(exit_station)
       expect(journey_log.journeys).to include(journey)
     end
